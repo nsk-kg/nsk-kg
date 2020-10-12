57a3a61f15ea533385427f9ee0c4aedf26b1ee8d
pogrebnyak.r <coder.ept@gmail.com>
2020-10-12 15:44:17 +0300

    Новые домкументы

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -231,6 +231,27 @@
                     </div>
                 </div>
             </div>
+
+            <div class="card">
+                <div class="card-header" id="headingThree8">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                data-target="#collapseThree8" aria-expanded="false" aria-controls="collapseThree8">
+                           Финансовые показатели
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseThree8" class="collapse" aria-labelledby="headingThree8" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <ul>
+                            <li><a href="/upload/Бух баланс за 2017 г с отметкой ГНИ.PDF">Бух баланс за 2017 г с отметкой ГНИ</a></li>
+                            <li><a href="/upload/Бухбаланс за 2018 г..PDF">Бухбаланс за 2018 г.</a></li>
+                            <li><a href="/upload/Баланс за декабрь 2019.pdf">Баланс за декабрь 2019</a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <h1 class="display-3">Наши клиенты</h1>
