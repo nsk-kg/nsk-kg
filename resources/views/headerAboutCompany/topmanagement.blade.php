baa34e8b155d3d5ee7ac95af90fda3f9add4ce14
adilbek <serikbayev.a@nsk.kz>
2022-02-22 14:52:04 +0600

    fix: npm fix

--- resources/views/headerAboutCompany/topmanagement.blade.php
+++ resources/views/headerAboutCompany/topmanagement.blade.php
@@ -1,53 +1,90 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="py-4 container">
-        <h1 class="display-sm-4">Топ-менеджмент</h1>
-        <div class="row">
-            <div class="col-md-4 col-xs-12 mt-3 mb-3">
-                <div class="card h-100">
-                    <img src="/img/18.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Кемелов Талантбек Эркинович</h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ПРЕДСЕДАТЕЛЬ СОВЕТА ДИРЕКТОРОВ</li>
-                    </ul>
-                    <div class="card-body">
-                        <a href="#" class="card-link">НСК — Надежное страхование</a>
-                    </div>
-                </div>
+    <div class="py-4 container" id="app">
+        <h1 class="display-sm-4 mb-4">Топ-менеджмент</h1>
+        <div class="manager has-img-left">
+            <figure class="manager-avatar"><img alt="" src="/img/18.png" /></figure>
+
+            <div class="manager-meta">
+                <div class="manager-name">Кемелов Талантбек Эркинович</div>
+
+                <div class="manager-position">Председатель Совета Директоров ЗАО «НСК»</div>
             </div>
-            <div class="col-md-4 col-xs-12 mt-3 mb-3">
-                <div class="card h-100">
-                    <img src="/img/19.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Ким Лариса Евгеньевна</h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ПРЕДСЕДАТЕЛЬ ПРАВЛЕНИЯ</li>
-                    </ul>
-                    <div class="card-body">
-                        <a href="#" class="card-link">НСК — Надежное страхование</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-xs-12 mt-3 mb-3">
-                <div class="card h-100">
-                    <img src="/img/20.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">Турумбеков Актан Орозбекович</h5>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ЗАМЕСТИТЕЛЬ ПРЕДСЕДАТЕЛЯ ПРАВЛЕНИЯ</li>
-                    </ul>
-                    <div class="card-body">
-                        <a href="#" class="card-link">НСК — Надежное страхование</a>
-                    </div>
-                </div>
+
+            <div class="manager-description">
+                <p style="margin:0cm 0cm 0.0001pt">Идейный управленец, сочетающий в себе порядочность и профессионализм</p>
+                <p style="margin:0cm 0cm 0.0001pt">Окончил Кыргызско-Турецкий Университет «Манас» по направлению «Менеджмент»
+                    и Кыргызский Национальный Университет, имени Жусупа Баласагына с квалификацией «Магистр делового Администрирования».</p>
+                <p style="margin:0cm 0cm 0.0001pt">Проходил сертификацию международного уровня Certificate in Insurance (Cert CII),
+                    The Chartesed Insurance Institute (London, UK).</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2008 году начал карьеру страховщика в ЗАО «Дочерняя организация «Нефтяная страховая
+                    компания», а с 2013 года возглавил отдел страхования и перестрахования.</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2013 г. – 2015 г. был руководителем технического отдела и отдела страхования жизни в ЗАО «London-Bishkek Insurance Company».</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2015 г.- 2016 г. – Председатель Правления ЗАО «А Плюс».</p>
+                <p style="margin:0cm 0cm 0.0001pt">Председатель Совета директоров ЗАО «НСК» с 2017 года.</p>
             </div>
         </div>
-    </div>
+        <div class="manager has-img-right">
+            <figure class="manager-avatar"><img alt="" src="/img/19.jpg" /></figure>
 
+            <div class="manager-meta">
+                <div class="manager-name">Ким Лариса Евгеньевна</div>
 
-@endsection
+                <div class="manager-position">Председатель Правления ЗАО «НСК»</div>
+            </div>
+
+            <div class="manager-description">
+                <p style="margin:0cm 0cm 0.0001pt">Профессиональный СЕО с 22-летним стажем работы в секторе страхования.</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 1999 году окончила Институт «Менеджмента, Бизнеса и Туризма Республиканского
+                    Центра подготовки предпринимателей» по специальности «Менеджмент».</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2000 г. - 2004 г. начала карьеру в страховой сфере в страховой компании АОЗТ «СК «Камкор-Лондон гейт».</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2004г. – 2005 г. работала в страховой компании ЗАО СК «Иншуренс Аян».</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2007 году – Исполнительный директор ЗАО СК «Камкор».</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2008 г. – 2016 г. была руководителем отдела прямого страхования ЗАО СК «Росстрах-Кыргызстан».</p>
+                <p style="margin:0cm 0cm 0.0001pt">Карьеру в ЗАО «НСК» начала в 2017 году на должности начальника отдела корпоративных продаж.
+                    Управляющий директор по развитию продаж филиальных и агентских сетей с 2019 года.</p>
+                <p style="margin:0cm 0cm 0.0001pt">Председатель Правления с апреля 2019 года.</p>
+            </div>
+        </div>
+        <div class="manager has-img-left">
+            <figure class="manager-avatar"><img alt="" src="/img/20.png" /></figure>
+
+            <div class="manager-meta">
+                <div class="manager-name">Турумбеков Актан Орозбекович</div>
+
+                <div class="manager-position">Заместитель Председателя Правления ЗАО «НСК»</div>
+            </div>
+
+            <div class="manager-description">
+                <p style="margin:0cm 0cm 0.0001pt">Профессионал с опытом работы в корпоративном страховании и перестраховании.</p>
+                <p style="margin:0cm 0cm 0.0001pt">Окончил Кыргызский Государственный Национальный Университет, имени Жусупа Баласагына
+                    в 1999 году по направлению «Экономика», в 2000 году по специальности «финансы и кредит».</p>
+                <p style="margin:0cm 0cm 0.0001pt">В 2002 г. – 2006 г. работал в Бишкекском Управлении Газового Хозяйства,
+                    с 2005 года заместителем руководителя службы по работе с населением.</p>
+                <p style="margin:0cm 0cm 0.0001pt">Осуществляет трудовую деятельность в компании с 2007 года.</p>
+                <p style="margin:0cm 0cm 0.0001pt">С 2009 г. по 2013 г. – Начальник отдела страхования и перестрахования ЗАО «Дочерняя организация «Нефтяная страховая компания».</p>
+                <p style="margin:0cm 0cm 0.0001pt">С 2013 г. по 2017 г. - Директор Департамента страхования и перестрахования ЗАО «НСК».</p>
+                <p style="margin:0cm 0cm 0.0001pt">Заместитель Председателя Правления с 2017 года.</p>
+            </div>
+        </div>
+        <div class="manager has-img-right">
+            <figure class="manager-avatar"><img alt="" src="/img/anon.jpg" /></figure>
+
+            <div class="manager-meta">
+                <div class="manager-name">Турдумбекова Чолпон Кушчубековна</div>
+
+                <div class="manager-position">Член Правления ЗАО «НСК»</div>
+            </div>
+
+            <div class="manager-description">
+                <p style="margin:0cm 0cm 0.0001pt">Непрерывный трудовой стаж успешной бухгалтерской деятельности с 2005 года.</p>
+                <p style="margin:0cm 0cm 0.0001pt">Окончила Кыргызский Национальный Университет, имени Жусупа Баласагына в 2001
+                    году по специальности «Математика» и в 2016 году по специальности «Бухгалтерский учет, анализ и аудит».</p>
+                <p style="margin:0cm 0cm 0.0001pt">С 2005 года работала в ЗСАО «Кыргызинстрах», с 2006 г. по 2009 г.
+                    на должностях бухгалтера-аналитика и бухгалтера.</p>
+                <p style="margin:0cm 0cm 0.0001pt">Назначена на должность главного бухгалтера ЗАО «НСК» в январе 2013 года.</p>
+                <p style="margin:0cm 0cm 0.0001pt">Член Правления с 2021 года.</p>
+            </div>
+        </div>
+    @endsection
